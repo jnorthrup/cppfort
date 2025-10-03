c34360cdf38c2056662e9ce319ce6d5d5b8b139c
Your Name <you@example.com>
2025-10-02 19:32:56 -0500

    chore: Clean up test artifacts from development

--- test_output_stdio.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() /*kinds:*/ {
-    std::string s = "Fred";
-    auto myfile = fopen("xyzzy", "w");
-    _ = myfile.fprintf( "Hello %s with UFCS!", s.c_str() );
-    _ = myfile.fclose();
-}
